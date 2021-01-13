c092b05bd7800631c3f417504dcb0c1b4aa8bd3a
sebastian Lopez Buritica <selobu@gmail.com>
2021-01-13 16:51:05 -0500

    adding a logo

---
+++ components/Logo.vue
@@ -0,0 +1,3 @@
+<template>
+  <v-img src="../assets/logo.png"></v-img>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="main text-center flex flex-col items-center justify-center">
-    <!-- <logo /> -->
+    <logo />
     <h1 class="title">GestionHSEQ</h1>
     <h2 class="subtitle">Seguridad, salud y medioambiente</h2>
   </div>
 </template>
 
 <script>
-// import Logo from '~/components/Logo.vue'
+import Logo from '~/components/Logo.vue'
 
 export default {
-  // components: {
-  //   Logo
-  // },
+  components: {
+    Logo,
+  },
   head() {
     return {
-      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }]
+      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
     }
-  }
+  },
 }
 </script>
